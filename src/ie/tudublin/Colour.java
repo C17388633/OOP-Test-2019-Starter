ce565d4b23889947907a47ed06ac5898dd10d55c
Killian Pogue <C17388633@mydit.ie>
2019-04-10 16:26:32 +0100

    Added the colour class with methods

---
+++ src/ie/tudublin/Colour.java
@@ -0,0 +1,92 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Colour
+{
+    private String colour;
+    private int r;
+    private int g;
+    private int b;
+    private int value;
+
+    public String toString()
+    {
+        return colour + "\t" + r + ", " + g + ", " + b + "\t" + value;
+    }
+
+    public Colour(TableRow tr)
+    {
+        this(tr.getString("colour"), tr.getInt("r"), tr.getInt("g"), tr.getInt("b"), tr.getInt("value"));
+    }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * @return the r
+     */
+    public int getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    /**
+     * @return the g
+     */
+    public int getG() {
+        return g;
+    }
+
+    /**
+     * @param g the g to set
+     */
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    /**
+     * @return the b
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
